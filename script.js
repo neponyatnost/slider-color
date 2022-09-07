b3cf8d1183f2de5f06f03689dce0642bb202d424
Ivan <iv.konuhov@gmail.com>
2022-09-07 19:00:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -39,19 +39,3 @@ function changeSlide(direction) {
     mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
     sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
 }
-function changeSlide(direction) {
-    if (direction === 'up') {
-        activeSlideIndex++
-        if (activeSlideIndex === slidesCount) {
-            activeSlideIndex = 0
-        }
-    } else if (direction === 'down') {
-        activeSlideIndex--
-        if (activeSlideIndex < 0) {
-            activeSlideIndex = slidesCount - 1
-        }
-    }
-    const height = container.clientHeight
-    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
-    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
-}
