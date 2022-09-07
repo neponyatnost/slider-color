c72aaa997a1d9ed47afe2a7096120492b691a3c0
Ivan <iv.konuhov@gmail.com>
2022-09-07 18:58:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,21 +13,6 @@ upBtn.addEventListener('click', () => {
 })
 downBtn.addEventListener('click', () => {
     changeSlide('down')
-})const upBtn = document.querySelector('.up-button')
-const downBtn = document.querySelector('.down-button')
-const sidebar = document.querySelector('.sidebar')
-const mainSlide = document.querySelector('.main-slide')
-const slidesCount = mainSlide.querySelectorAll('div').length
-const container = document.querySelector('.container')
-let activeSlideIndex = 0
-
-sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
-
-upBtn.addEventListener('click', () => {
-    changeSlide('up')
-})
-downBtn.addEventListener('click', () => {
-    changeSlide('down')
 })
 
 document.addEventListener('keydown', function (e) {
